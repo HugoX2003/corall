3e5ee15e62dd113098c67d7141bbea1558dd942f
Hugo <hugomd2003xd@gmail.com>
2023-11-29 15:39:39 -0500

    corall

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-//mensaje de autentificación de usuario
-
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
     let id = document.getElementById('id').value;
     let password = document.getElementById('password').value;
-    // Aquí puedes validar el ID y la contraseña contra una base de datos o algún otro sistema de autenticación
-    alert('Usuario autenticado con éxito. ID: ' + id + ', Contraseña: ' + password);
+
+    // Aquí puedes agregar la lógica de autenticación
+    // Puedes cambiar esto por tu lógica real de autenticación
+    let authenticated = authenticateUser(id, password);
+
+    if (authenticated) {
+        // Usuario autenticado con éxito
+        if (id === 'empleado') {
+            alert('Empleado autenticado con éxito.');
+            window.location.href = 'empleado/empleadoprincipal.html';
+        } else if (id === 'gerente') {
+            alert('Gerente autenticado con éxito.');
+            window.location.href = 'gerente/gerenteprincipal.html';
+        } else {
+            alert('Usuario autenticado con éxito.');
+        }
+    } else {
+        alert('Error de autenticación. Por favor, verifica tu ID y contraseña.');
+    }
 });
 
+function authenticateUser(id, password) {
+    // Aquí deberías implementar tu lógica real de autenticación
+    // Por ahora, simplemente devolvemos true para propósitos de ejemplo
+    return true;
+}
